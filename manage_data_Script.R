293a2a6c73a80e637f37f05fd27dc3757bc220bc
pogacha <89231282@student.upr.si>
2024-07-04 14:33:53 +0200

    "csv?"

--- manage_data_Script.R
+++ manage_data_Script.R
@@ -3,18 +3,13 @@ library(writexl)
 library(readxl)
 
 # open data
-read_xlsx(path = "progAAAAAAA/nuggets/nuggetsData.xlsx",
-          col_names = T) -> nuggets
+# read_xlsx(dir(pattern = "nuggetsData.xlsx", recursive = TRUE), 
+#           col_names = T) -> nuggets
+read.csv(dir(pattern="nuggetsData.csv", recursive = TRUE)) -> nuggets;nuggets
 
-nuggets %>% 
-  as_tibble()
+# ...
+# 
+# ...
 
-
-
-# ! ! !
-# write - PAZI, DA NE RUNNAŠ TEGA PONESREČI
-# ! ! !
-nuggets %>% 
-  write_xlsx(path = "progAAAAAAA/nuggets/nuggetsData.xlsx",
-             col_names = T,
-             format_headers = T)
+#nuggets %>%  write_xlsx(path = "progAAAAAAA/nuggets/nuggetsData.xlsx",col_names = T, format_headers = T)
+#nuggets %>%  write.csv(file="nuggetsData.csv")
